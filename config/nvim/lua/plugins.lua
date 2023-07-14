4dc775d01f6436168a46b39d4d85694f2ab6c18a
Diego Toral <diegotoral@gmail.com>
2023-07-14 10:09:06 -0700

    Ensure packer is installed and loaded

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,5 +1,18 @@
 vim.cmd([[autocmd BufWritePost plugins.lua source <afile> | PackerCompile]])
 
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 return require('packer').startup(function()
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
@@ -91,4 +104,8 @@ return require('packer').startup(function()
   use { 'AndrewRadev/splitjoin.vim' }
 
   use { '~/code/nvim-rspec' }
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
